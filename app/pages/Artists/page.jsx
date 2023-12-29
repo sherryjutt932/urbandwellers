efe0f777fea9e49b517b02ac83928d611d21e6dc
sherryjutt932 <ladlajutt442@gmail.com>
2023-12-29 09:21:04 +0500

    new code

--- app/pages/Artists/page.jsx
+++ app/pages/Artists/page.jsx
@@ -5,11 +5,7 @@ import ArtistsFilter from "@/app/constants/ArtistsFilter";
 
 export default function Artists() {
   return (
-<<<<<<< HEAD
-    <div className='flex flex-nowrap w-fit h-full sm:pt-10'>
-=======
     <div className='flex flex-nowrap w-fit h-full pt-10 sm:pt-0'>
->>>>>>> 96afd09dfca5fc454fd6a6fcb19cb10f0854b0d5
         <Filter item={ArtistsFilter} name="artists"/>
       {
         ArtistsList.map((item, index)=>{
--- app/pages/Events/page.jsx
+++ app/pages/Events/page.jsx
@@ -5,11 +5,7 @@ import EventsFilter from "@/app/constants/EventsFilter";
 
 export default function Events() {
   return (
-<<<<<<< HEAD
-    <div className='flex flex-nowrap w-fit h-full sm:pt-10'>
-=======
     <div className='flex flex-nowrap w-fit h-full pt-10 sm:pt-0'>
->>>>>>> 96afd09dfca5fc454fd6a6fcb19cb10f0854b0d5
         <Filter item={EventsFilter} name="events"/>
       {
         EventsList.map((item, index)=>{
