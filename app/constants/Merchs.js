cc17f61944dad94dae6087e7b4b4355713063e9f
sherryjutt932 <ladlajutt442@gmail.com>
2024-01-25 03:18:33 +0500

    latest changes

--- app/constants/Merchs.js
+++ app/constants/Merchs.js
@@ -1,4 +1,41 @@
 const Merch = [
+
+  {
+    category: "Towel",
+    timestamp: "2024-01-19T12:30:00Z",
+    title: "Beach Towel.webp",
+    price: "$32.03",
+    url: "https://urbandwellerlifestyle.myshopify.com/products/beach-towel",
+    img: "/Merch/Beach Towel.webp",
+    soldout: false
+  },
+  {
+    category: "shirt",
+    timestamp: "2024-01-23T12:30:00Z",
+    title: "SDL t shirt",
+    price: "$35.00",
+    url: "https://urbandwellerlifestyle.myshopify.com/products/sdl-t-shirt",
+    img: "/Merch/SDL t shirt.webp",
+    soldout: false
+  },
+  {
+    category: "Sweatshirt",
+    timestamp: "2024-01-24T12:30:00Z",
+    title: "Urban Dwellers Music Unisex Heavy Blend Crewneck Sweatshirt",
+    price: "$33.73",
+    url: "https://urbandwellerlifestyle.myshopify.com/products/urban-dwellers-music-unisex-heavy-blend%E2%84%A2-crewneck-sweatshirt",
+    img: "/Merch/Urban Dwellers Music Unisex Heavy Blend Crewneck Sweatshirt.webp",
+    soldout: false
+  },
+  {
+    category: "Poster",
+    timestamp: "2024-01-20T12:30:00Z",
+    title: "Fine Art Posters.webp",
+    price: "$11.22",
+    url: "https://urbandwellerlifestyle.myshopify.com/products/fine-art-posters-2",
+    img: "/Merch/Fine Art Posters.webp",
+    soldout: false
+  },
     {
       category: "shirt",
       timestamp: "2024-01-13T12:30:00Z",
@@ -8,51 +45,51 @@ const Merch = [
       img: "/Merch/After Life shirt.jpg",
       soldout: false
     },
-    {
-      category: "bandana",
-      title: "All-over print bandana",
-      timestamp: "2024-01-10T12:30:00Z",
-      price: "$13.00",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/all-over-print-bandana",
-      img: "/Merch/All-over print bandana.jpg",
-      soldout: false
-    },
-    {
-      category: "bandana",
-      timestamp: "2024-01-03T12:30:00Z",
-      title: "All-over print bandana",
-      price: "$15.00",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/all-over-print-bandana-1",
-      img: "/Merch/All-over print bandana 2.jpg",
-      soldout: false
-    },
-    {
-      category: "bandana",
-      timestamp: "2024-01-01T12:30:00Z",
-      title: "All-over print bandana",
-      price: "$13.00",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/all-over-print-bandana-2",
-      img: "/Merch/All-over print bandana 3.jpg",
-      soldout: false
-    },
-    {
-      category: "pillow",
-      timestamp: "2023-12-13T12:30:00Z",
-      title: "Basic Pillow",
-      price: "$22.00",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/basic-pillow",
-      img: "/Merch/Basic Pillow.jpg",
-      soldout: false
-    },
-    {
-      category: "towel",
-      timestamp: "2023-12-23T12:30:00Z",
-      title: "Beach Towel",
-      price: "$32.03",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/basic-pillow",
-      img: "/Merch/Beach Towel.jpg",
-      soldout: false
-    },
+    // {
+    //   category: "bandana",
+    //   title: "All-over print bandana",
+    //   timestamp: "2024-01-10T12:30:00Z",
+    //   price: "$13.00",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/all-over-print-bandana",
+    //   img: "/Merch/All-over print bandana.jpg",
+    //   soldout: false
+    // },
+    // {
+    //   category: "bandana",
+    //   timestamp: "2024-01-03T12:30:00Z",
+    //   title: "All-over print bandana",
+    //   price: "$15.00",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/all-over-print-bandana-1",
+    //   img: "/Merch/All-over print bandana 2.jpg",
+    //   soldout: false
+    // },
+    // {
+    //   category: "bandana",
+    //   timestamp: "2024-01-01T12:30:00Z",
+    //   title: "All-over print bandana",
+    //   price: "$13.00",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/all-over-print-bandana-2",
+    //   img: "/Merch/All-over print bandana 3.jpg",
+    //   soldout: false
+    // },
+    // {
+    //   category: "pillow",
+    //   timestamp: "2023-12-13T12:30:00Z",
+    //   title: "Basic Pillow",
+    //   price: "$22.00",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/basic-pillow",
+    //   img: "/Merch/Basic Pillow.jpg",
+    //   soldout: false
+    // },
+    // {
+    //   category: "towel",
+    //   timestamp: "2023-12-23T12:30:00Z",
+    //   title: "Beach Towel",
+    //   price: "$32.03",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/basic-pillow",
+    //   img: "/Merch/Beach Towel.jpg",
+    //   soldout: false
+    // },
     {
       category: "mug",
       timestamp: "2023-11-23T12:30:00Z",
@@ -62,24 +99,24 @@ const Merch = [
       img: "/Merch/Black Glossy Mug.jpg",
       soldout: false
     },
-    {
-      category: "bag",
-      timestamp: "2023-11-23T12:30:00Z",
-      title: "Clutch Bag",
-      price: "$27.83",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/clutch-bag",
-      img: "/Merch/Clutch Bag.jpg",
-      soldout: false
-    },
-    {
-      category: "bag",
-      title: "Crossbody bag",
-      timestamp: "2023-11-23T12:30:00Z",
-      price: "$39.00",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/crossbody-bag",
-      img: "/Merch/Crossbody bag.jpg",
-      soldout: false
-    },
+    // {
+    //   category: "bag",
+    //   timestamp: "2023-11-23T12:30:00Z",
+    //   title: "Clutch Bag",
+    //   price: "$27.83",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/clutch-bag",
+    //   img: "/Merch/Clutch Bag.jpg",
+    //   soldout: false
+    // },
+    // {
+    //   category: "bag",
+    //   title: "Crossbody bag",
+    //   timestamp: "2023-11-23T12:30:00Z",
+    //   price: "$39.00",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/crossbody-bag",
+    //   img: "/Merch/Crossbody bag.jpg",
+    //   soldout: false
+    // },
     {
       category: "cap",
       timestamp: "2023-11-23T12:30:00Z",
@@ -89,15 +126,15 @@ const Merch = [
       img: "/Merch/GDR Knit Beanie.jpg",
       soldout: false
     },
-    {
-      category: "bag",
-      title: "GDR Makeup Bag",
-      timestamp: "2023-11-23T12:30:00Z",
-      price: "$19.90",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/gdr-makeup-bag",
-      img: "/Merch/GDR Makeup Bag.jpg",
-      soldout: false
-    },
+    // {
+    //   category: "bag",
+    //   title: "GDR Makeup Bag",
+    //   timestamp: "2023-11-23T12:30:00Z",
+    //   price: "$19.90",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/gdr-makeup-bag",
+    //   img: "/Merch/GDR Makeup Bag.jpg",
+    //   soldout: false
+    // },
     {
       category: "poster",
       timestamp: "2023-11-23T12:30:00Z",
@@ -125,24 +162,24 @@ const Merch = [
       img: "/Merch/Hip Hop Culture Coin 1974 - 2014.jpg",
       soldout: false
     },
-    {
-      category: "cap",
-      timestamp: "2023-11-23T12:30:00Z",
-      title: "Knit Beanie",
-      price: "$24.45",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/knit-beanie",
-      img: "/Merch/Knit Beanie.jpg",
-      soldout: false
-    },
-    {
-      category: "pillow",
-      timestamp: "2023-11-23T12:30:00Z",
-      title: "Premium Pillow",
-      price: "$25.00",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/premium-pillow",
-      img: "/Merch/Premium Pillow.jpg",
-      soldout: false
-    },
+    // {
+    //   category: "cap",
+    //   timestamp: "2023-11-23T12:30:00Z",
+    //   title: "Knit Beanie",
+    //   price: "$24.45",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/knit-beanie",
+    //   img: "/Merch/Knit Beanie.jpg",
+    //   soldout: false
+    // },
+    // {
+    //   category: "pillow",
+    //   timestamp: "2023-11-23T12:30:00Z",
+    //   title: "Premium Pillow",
+    //   price: "$25.00",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/premium-pillow",
+    //   img: "/Merch/Premium Pillow.jpg",
+    //   soldout: false
+    // },
     {
       category: "rug",
       timestamp: "2023-11-23T12:30:00Z",
@@ -179,15 +216,15 @@ const Merch = [
       img: "/Merch/SDL t shirt.jpg",
       soldout: false
     },
-    {
-      category: "shadow",
-      timestamp: "2023-11-23T12:30:00Z",
-      title: "Systematic Drum Lords - Shadows",
-      price: "$1.00",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/sdl-t-shirt",
-      img: "/Merch/Systematic Drum Lords - Shadows.png",
-      soldout: false
-    },
+    // {
+    //   category: "shadow",
+    //   timestamp: "2023-11-23T12:30:00Z",
+    //   title: "Systematic Drum Lords - Shadows",
+    //   price: "$1.00",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/sdl-t-shirt",
+    //   img: "/Merch/Systematic Drum Lords - Shadows.png",
+    //   soldout: false
+    // },
     {
       category: "tee",
       timestamp: "2023-11-23T12:30:00Z",
@@ -197,52 +234,52 @@ const Merch = [
       img: "/Merch/Systematic Drum Lords Unisex Jersey Long Sleeve Tee.jpg",
       soldout: false
     },
-    {
-      category: "tee",
-      timestamp: "2023-11-23T12:30:00Z",
-      title: "Ultra Cotton Long Sleeve Tee",
-      price: "$22.53",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/ultra-cotton-long-sleeve-tee",
-      img: "/Merch/Ultra Cotton Long Sleeve Tee.jpg",
-      soldout: false
-    },
-    {
-      category: "jacket",
-      timestamp: "2023-11-23T12:30:00Z",
-      title: "Unisex Bomber Jacket",
-      price: "$53.00",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/unisex-bomber-jacket",
-      img: "/Merch/Unisex Bomber Jacket.jpg",
-      soldout: false
-    },
-    {
-      category: "tee",
-      timestamp: "2023-11-23T12:30:00Z",
-      title: "Unisex Jersey Long Sleeve Tee",
-      price: "$29.90",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/unisex-bomber-jacket",
-      img: "/Merch/Unisex Jersey Long Sleeve Tee.jpg",
-      soldout: false
-    },
-    {
-      category: "tank",
-      timestamp: "2023-11-23T12:30:00Z",
-      title: "Unisex Jersey Tank",
-      price: "$22.18",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/unisex-bomber-jacket",
-      img: "/Merch/Unisex Jersey Tank.jpg",
-      soldout: false
-    },
-    {
-      category: "pant",
-      timestamp: "2023-11-23T12:30:00Z",
-      title: "Unisex track pants",
-      price: "$52.00",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/unisex-track-pants",
-      img: "/Merch/Unisex track pants.jpg",
-      soldout: false
+    // {
+    //   category: "tee",
+    //   timestamp: "2023-11-23T12:30:00Z",
+    //   title: "Ultra Cotton Long Sleeve Tee",
+    //   price: "$22.53",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/ultra-cotton-long-sleeve-tee",
+    //   img: "/Merch/Ultra Cotton Long Sleeve Tee.jpg",
+    //   soldout: false
+    // },
+    // {
+    //   category: "jacket",
+    //   timestamp: "2023-11-23T12:30:00Z",
+    //   title: "Unisex Bomber Jacket",
+    //   price: "$53.00",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/unisex-bomber-jacket",
+    //   img: "/Merch/Unisex Bomber Jacket.jpg",
+    //   soldout: false
+    // },
+    // {
+    //   category: "tee",
+    //   timestamp: "2023-11-23T12:30:00Z",
+    //   title: "Unisex Jersey Long Sleeve Tee",
+    //   price: "$29.90",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/unisex-bomber-jacket",
+    //   img: "/Merch/Unisex Jersey Long Sleeve Tee.jpg",
+    //   soldout: false
+    // },
+    // {
+    //   category: "tank",
+    //   timestamp: "2023-11-23T12:30:00Z",
+    //   title: "Unisex Jersey Tank",
+    //   price: "$22.18",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/unisex-bomber-jacket",
+    //   img: "/Merch/Unisex Jersey Tank.jpg",
+    //   soldout: false
+    // },
+    // {
+    //   category: "pant",
+    //   timestamp: "2023-11-23T12:30:00Z",
+    //   title: "Unisex track pants",
+    //   price: "$52.00",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/unisex-track-pants",
+    //   img: "/Merch/Unisex track pants.jpg",
+    //   soldout: false
 
-    },
+    // },
     {
       category: "wallet",
       timestamp: "2023-11-23T12:30:00Z",
@@ -253,26 +290,26 @@ const Merch = [
       soldout: false
 
     },
-    {
-      category: "sticker",
-      timestamp: "2023-11-23T12:30:00Z",
-      title: "Urban Dwellers reflective sticker",
-      price: "$10.00",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/urban-dwellers-reflective-sticker",
-      img: "/Merch/Urban Dwellers reflective sticker.jpg",
-      soldout: true
+    // {
+    //   category: "sticker",
+    //   timestamp: "2023-11-23T12:30:00Z",
+    //   title: "Urban Dwellers reflective sticker",
+    //   price: "$10.00",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/urban-dwellers-reflective-sticker",
+    //   img: "/Merch/Urban Dwellers reflective sticker.jpg",
+    //   soldout: true
 
-    },
-    {
-      category: "jersey",
-      timestamp: "2023-11-23T12:30:00Z",
-      title: "Urban Dwellers Unisex Basketball Jersey (AOP)",
-      price: "$32.33",
-      url: "https://urbandwellerlifestyle.myshopify.com/products/urban-dwellers-unisex-basketball-jersey-aop",
-      img: "/Merch/Urban Dwellers Unisex Basketball Jersey (AOP).jpg",
-      soldout: false
+    // },
+    // {
+    //   category: "jersey",
+    //   timestamp: "2023-11-23T12:30:00Z",
+    //   title: "Urban Dwellers Unisex Basketball Jersey (AOP)",
+    //   price: "$32.33",
+    //   url: "https://urbandwellerlifestyle.myshopify.com/products/urban-dwellers-unisex-basketball-jersey-aop",
+    //   img: "/Merch/Urban Dwellers Unisex Basketball Jersey (AOP).jpg",
+    //   soldout: false
 
-    },
+    // },
     {
       category: "pin",
       timestamp: "2023-11-23T12:30:00Z",
@@ -281,7 +318,8 @@ const Merch = [
       url: "https://urbandwellerlifestyle.myshopify.com/products/uzn-pin",
       img: "/Merch/UZN pin.jpg",
       soldout: false
-    }
+    },
+    
     
   ];
   
